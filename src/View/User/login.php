2340aea1565b9d86b0df7a6e69dab6ac40d08ee3
Marine <marine.moynet@epitech.eu>
2019-02-25 14:35:01 +0100

    Aborescence

---
+++ src/View/User/login.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Formulaire</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body>
+    <form action="/index.php" method="POST">
+        <input type="email" placeholder="email">
+        <input type="password" placeholder="password">
+        <button type="submit" value="Go"></button>
+    </form>
+</body>
+</html>
