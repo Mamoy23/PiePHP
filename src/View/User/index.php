ab892ad2db0d345239e5fd3b3a05b3effff4b5d5
Marine <marine.moynet@epitech.eu>
2019-02-25 16:18:52 +0100

    test

---
+++ src/View/User/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <title>Index</title>
+</head>
+<body>
+    <pre>
+        <?php var_dump($_POST);
+        var_dump($_GET);
+        var_dump($_SERVER);?>
+    </pre>    
+</body>
+</html>
--- src/View/User/login.php
+++ src/View/User/login.php
@@ -2,15 +2,13 @@
 <html>
 <head>
     <meta charset="utf-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>Formulaire</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 <body>
-    <form action="/index.php" method="POST">
-        <input type="email" placeholder="email">
-        <input type="password" placeholder="password">
-        <button type="submit" value="Go"></button>
+    <form action="index.php" method="POST">
+        <input type="email" name="email" placeholder="email">
+        <input type="password" name="password" placeholder="password">
+        <button type="submit">Go</button>
     </form>
 </body>
-</html>
+</html> 
